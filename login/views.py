5146e68d8976f104bad0905cd5f3ba7cd32aec28
Eric <eric@Intersect.(none)>
2013-02-10 20:49:22 -0800

    warmup

--- login/views.py
+++ login/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from login.models import UsersModel
 import login.models
 from django.shortcuts import render_to_response
-
+from django.core.context_processors import csrf
 
 def index(request):
     c = {}
