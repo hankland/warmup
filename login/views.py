593b13c3562ad2c48adcf8514965ff41144c1045
Eric <eric@Intersect.(none)>
2013-02-10 20:47:41 -0800

    warmup

--- login/views.py
+++ login/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('client.html', {})
+    c = {}
+    c.update(csrf(request)
+    return render_to_response('client.html', c)
 
 def login(request):
     return HttpResponse("<html><body>login</body></html>")
