c451e9a0a0a858fc1ec4810b352f25f4c276b182
Eric <eric@Intersect.(none)>
2013-02-10 19:38:12 -0800

    warmup

--- warmup/urls.py
+++ warmup/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     url(r'^$', 'login.views.index'),
     # Examples:
     # url(r'^$', 'warmup.views.home', name='home'),
