417bd8b02b4c016c97b1b0648e4c4b12d8ece773
Eric <eric@Intersect.(none)>
2013-02-19 02:29:57 -0800

    updatednewclient

---
+++ warmup/static/newclient.js
@@ -0,0 +1,17 @@
+ERR_BAD_CREDENTIALS = (-1);
+ERR_USER_EXISTS = (-2);
+ERR_BAD_USERNAME = (-3);
+ERR_BAD_PASSWORD  = (-4);
+
+function one() {
+    alert('works1');
+}
+
+function two() {
+    alert('works2');
+}
+
+function three() {
+    alert('works3');
+}
+
