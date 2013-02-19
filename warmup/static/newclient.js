354c00460d1ed6357dddc476877da92f83e40ba9
Eric <eric@Intersect.(none)>
2013-02-19 03:31:41 -0800

    updatednewclient

--- warmup/static/newclient.js
+++ warmup/static/newclient.js
@@ -3,13 +3,55 @@ ERR_USER_EXISTS = (-2);
 ERR_BAD_USERNAME = (-3);
 ERR_BAD_PASSWORD  = (-4);
 
+function set_message(err) {
+    if (err == -1) {
+        $('#message').val("Invalid username and password combination. Please try again. ");
+    } else if (err == -2) {
+        $('#message').val("This user name already exists. Please try again.");
+    } else if (err == -3) {
+        $('#message').val("The user name should not be empty and at most 128 characters long. Please try again.");
+    } else if (err == -4) {
+        $('#message').val("The password should be at most 128 characters long. Please try again");
+    }
+}
+
+function on_return(data) {
+   if( data.errCode > 0) {
+       $('#count').html(data.count);
+       $('#page1').hide()
+       $('#page2').show()
+   } else {
+       set_message(data.errCode);
+       $('#username').val("");
+       $('#password').val("");
+   }
+}
+
 function one() {
-    alert('works1');
+    $.ajax({
+        type: 'POST',
+        url: '/users/login',
+        data: JSON.stringify({ user: $('#username').val(), password : $('#password').val()}),
+        contentType: "application/json",
+        dataType: "json",
+        success: on_return
+        error: alert("bad request")
+        });
+    $('#user').html($('#username').val());
+    
 }
 
 function two() {
-    $('#page1').hide();
-    $('#page2').show();
+    $.ajax({
+        type: 'POST',
+        url: '/users/login',
+        data: JSON.stringify({ user: $('#username').val(), password : $('#password').val()}),
+        contentType: "application/json",
+        dataType: "json",
+        success: on_return
+        error: alert("bad request") 
+        });
+    $('#user').html($('#username').val());
 }
 
 function three() {
